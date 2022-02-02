aaa20169e42a45ba5eebfd76ea5527b5bf56ff9a
tl1071 <tl1071@bu.edu>
2022-02-02 09:46:49 -0500

    try push

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -13,7 +13,7 @@ import "strconv"
 // example to show how to declare the arguments
 // and reply for an RPC.
 //
-
+////try here
 type ExampleArgs struct {
 	X int
 }
@@ -23,7 +23,6 @@ type ExampleReply struct {
 }
 
 // Add your RPC definitions here.
-
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
